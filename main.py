952508b85fc6302f25c9933d518bf0395015f15d
limberruiz2-cpu <limberruiz2@gmail.com>
2026-05-29 18:23:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+﻿import requests
+
+BOT_TOKEN = "8878074016:AAE4VaVQ2D-XzXm3uWU90trey85rt7rEhUU"
+CHAT_ID = "5855497862"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+r = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "Prueba desde Python"
+    }
+)
+
+print(r.text)
